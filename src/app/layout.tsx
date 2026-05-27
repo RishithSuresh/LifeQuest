605264a8ae548996c66ac8ffb6f24c0d5721ad29
RishithSuresh <rishithsuresh10@gmail.com>
2026-05-27 09:57:46 +0530

    Package Changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full antialiased">
-      <body className="min-h-full flex flex-col bg-[#06080f]">{children}</body>
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="min-h-full flex flex-col bg-[#06080f]" suppressHydrationWarning>
+        {children}
+      </body>
     </html>
   );
 }
